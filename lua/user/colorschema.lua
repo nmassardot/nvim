e6272846b20f1389a189c2d022cce74f906464ce
Nicolás Massardo <nmassardot@uc.cl>
2022-09-05 14:38:19 -0400

    feat(config): change default theme and blame format

--- lua/user/colorschema.lua
+++ lua/user/colorschema.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme spacedark
+  colorscheme lunar
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -21,6 +21,7 @@ gitsigns.setup {
   },
   attach_to_untracked = true,
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
