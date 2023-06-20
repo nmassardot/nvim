9a035a728f5662c3dd0594e52a1e5eae34251c1a
Nicolas Massardo <nmassardotapia@gmail.com>
2023-06-20 14:12:19 -0400

    fix(whichkey): remove unused option

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -83,7 +83,6 @@ local mappings = {
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-  ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
   p = {
     name = "Packer",
