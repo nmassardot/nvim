15cfb9c88aeab42e6ad4ba4f4b460c20b04a1b52
Nicolas Massardo <nmassardotapia@gmail.com>
2023-06-20 15:06:42 -0400

    feat(null-ls): setup pyright formatting

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,7 +1,9 @@
-local status_ok, _ = pcall(require, "lspconfig")
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
   return
 end
+
+lspconfig.pyright.setup{}
 
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
